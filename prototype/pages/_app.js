1b1a064006150fecd2769c7760523ec770a4946a
marco79423 <marco79423@gmail.com>
2020-12-09 22:41:52 +0800

    更新 prototype

--- prototype/pages/_app.js
+++ prototype/pages/_app.js
@@ -1,7 +1,11 @@
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+
+function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default App
--- prototype/pages/index.js
+++ prototype/pages/index.js
@@ -1,65 +1,201 @@
+import React from 'react'
+import AppBar from '@material-ui/core/AppBar'
+import Button from '@material-ui/core/Button'
+import CameraIcon from '@material-ui/icons/PhotoCamera'
+import Card from '@material-ui/core/Card'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
+import CardMedia from '@material-ui/core/CardMedia'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import Grid from '@material-ui/core/Grid'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import {makeStyles} from '@material-ui/core/styles'
+import Container from '@material-ui/core/Container'
+import Link from '@material-ui/core/Link'
+
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  )
+}
+
+const useStyles = makeStyles((theme) => ({
+  icon: {
+    marginRight: theme.spacing(2),
+  },
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6),
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4),
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  cardMedia: {
+    paddingTop: '56.25%', // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(6),
+  },
+}))
+
 
 export default function Home() {
+  const classes = useStyles()
+
+  const gospels = [
+    {name: 'Nabi姐', message: '你怎麼長這樣, 笑死'},
+    {name: 'Nabi姐', message: '我不是在教你, 我是在打臉你'},
+    {
+      name: '威廉哥', message: '大家好 我是新來的垃圾\n' +
+        '          請各位前輩盡量打我罵我幹我蹂躪我\n' +
+        '          反正我就是不值一提的低能廢物'
+    },
+    {name: 'Nabi姐', message: '你怎麼長這樣, 笑死'},
+    {name: 'Nabi姐', message: '我不是在教你, 我是在打臉你'},
+    {
+      name: '威廉哥', message: '大家好 我是新來的垃圾\n' +
+        '          請各位前輩盡量打我罵我幹我蹂躪我\n' +
+        '          反正我就是不值一提的低能廢物'
+    },
+    {name: 'Nabi姐', message: '你怎麼長這樣, 笑死'},
+    {name: 'Nabi姐', message: '我不是在教你, 我是在打臉你'},
+    {
+      name: '威廉哥', message: '大家好 我是新來的垃圾\n' +
+        '          請各位前輩盡量打我罵我幹我蹂躪我\n' +
+        '          反正我就是不值一提的低能廢物'
+    },
+    {name: 'Nabi姐', message: '你怎麼長這樣, 笑死'},
+    {name: 'Nabi姐', message: '我不是在教你, 我是在打臉你'},
+    {
+      name: '威廉哥', message: '大家好 我是新來的垃圾\n' +
+        '          請各位前輩盡量打我罵我幹我蹂躪我\n' +
+        '          反正我就是不值一提的低能廢物'
+    },
+    {name: 'Nabi姐', message: '你怎麼長這樣, 笑死'},
+    {name: 'Nabi姐', message: '我不是在教你, 我是在打臉你'},
+    {
+      name: '威廉哥', message: '大家好 我是新來的垃圾\n' +
+        '          請各位前輩盡量打我罵我幹我蹂躪我\n' +
+        '          反正我就是不值一提的低能廢物'
+    },
+  ]
+
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico"/>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"/>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+      <>
+        <CssBaseline/>
+        <AppBar position="relative">
+          <Toolbar>
+            <CameraIcon className={classes.icon}/>
+            <Typography variant="h6" color="inherit" noWrap>
+              Album layout
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <main>
+          {/* Hero unit */}
+          <div className={classes.heroContent}>
+            <Container maxWidth="sm">
+              <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+                Album layout
+              </Typography>
+              <Typography variant="h5" align="center" color="textSecondary" paragraph>
+                Something short and leading about the collection below—its contents, the creator, etc.
+                Make it short and sweet, but not too short so folks don&apost simply skip over it
+                entirely.
+              </Typography>
+              <div className={classes.heroButtons}>
+                <Grid container spacing={2} justify="center">
+                  <Grid item>
+                    <Button variant="contained" color="primary">
+                      Main call to action
+                    </Button>
+                  </Grid>
+                  <Grid item>
+                    <Button variant="outlined" color="primary">
+                      Secondary action
+                    </Button>
+                  </Grid>
+                </Grid>
+              </div>
+            </Container>
+          </div>
+          <Container className={classes.cardGrid} maxWidth="md">
+            {/* End hero unit */}
+            <Grid container spacing={4}>
+              {gospels.map((gospel, i) => (
+                <Grid item key={i} xs={12} sm={6} md={4}>
+                  <Card className={classes.card}>
+                    <CardMedia
+                      className={classes.cardMedia}
+                      image="https://source.unsplash.com/random"
+                      title="Image title"
+                    />
+                    <CardContent className={classes.cardContent}>
+                      <Typography gutterBottom variant="h5" component="h2">
+                        {gospel.name}
+                      </Typography>
+                      <Typography>
+                         {gospel.message}
+                      </Typography>
+                    </CardContent>
+                    <CardActions>
+                      <Button size="small" color="primary">
+                        修改
+                      </Button>
+                      <Button size="small" color="primary">
+                        刪除
+                      </Button>
+                    </CardActions>
+                  </Card>
+                </Grid>
+              ))}
+            </Grid>
+          </Container>
+        </main>
+        {/* Footer */}
+        <footer className={classes.footer}>
+          <Typography variant="h6" align="center" gutterBottom>
+            Footer
+          </Typography>
+          <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
+            Something here to give the footer a purpose!
+          </Typography>
+          <Copyright/>
+        </footer>
+        {/* End footer */}
+      </>
+    </>
   )
 }
