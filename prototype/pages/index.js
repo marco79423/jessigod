86aaaced034deaad25e180315b66a6c04f897a59
marco79423 <marco79423@gmail.com>
2020-12-12 20:46:31 +0800

    更新 prototype

--- prototype/pages/index.js
+++ prototype/pages/index.js
@@ -10,7 +10,7 @@ import CssBaseline from '@material-ui/core/CssBaseline'
 import Grid from '@material-ui/core/Grid'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
-import {makeStyles} from '@material-ui/core/styles'
+import {makeStyles, withStyles} from '@material-ui/core/styles'
 import Container from '@material-ui/core/Container'
 import Link from '@material-ui/core/Link'
 import Head from 'next/head'
@@ -20,52 +20,60 @@ import Slide from '@material-ui/core/Slide';
 import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
 import Fab from '@material-ui/core/Fab';
 import Accordion from '@material-ui/core/Accordion';
-import AccordionSummary from '@material-ui/core/AccordionSummary';
-import AccordionDetails from '@material-ui/core/AccordionDetails';
+import MuiAccordionSummary from '@material-ui/core/AccordionSummary';
+import MuiAccordionDetails from '@material-ui/core/AccordionDetails';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
+  const randomKey = Math.random().toString(36).substring(7);
+
+const AccordionSummary = withStyles({
+  content: {
+    flexDirection: 'column'
+  }
+})(MuiAccordionSummary)
+
+const AccordionDetails = withStyles((theme) => ({
+  root: {
+    backgroundColor: theme.palette.action.hover,
+  },
+}))(MuiAccordionDetails);
+
+const SimpleAccordionStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+  },
+  content: {
+    fontSize: 1000,
+  }
+}));
+
 function SimpleAccordion() {
-  const useStyles = makeStyles((theme) => ({
-    root: {
-      width: '100%',
-    },
-    heading: {
-      fontSize: theme.typography.pxToRem(15),
-      fontWeight: theme.typography.fontWeightRegular,
-    },
-  }));
-  const classes = useStyles();
+
+  const classes = SimpleAccordionStyles();
 
   return (
     <div className={classes.root}>
       <Accordion expanded>
         <AccordionSummary
           expandIcon={<ExpandMoreIcon/>}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
+          classes={classes.summary}
         >
-          <Typography className={classes.heading}>如何聽見福音？</Typography>
+          <Typography variant='h5'>如何聽見福音？</Typography>
+          <Typography style={{marginTop: 8, lineHeight: 2}}>西卡神是博愛的，祂允許所有神、智者、甚至人渣都有平等說話的權利。<br/>西卡神會紀錄每個人說的話，並將這些話語平等的傳遞給每一個教徒。</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-            sit amet blandit leo lobortis eget.
-          </Typography>
+          <Typography>每天九點，西卡神會自動推送大家說的話到各個平台 （目前支援 slack、telegram、line 平台）</Typography>
         </AccordionDetails>
       </Accordion>
-      <Accordion expanded>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon/>}
-          aria-controls="panel2a-content"
-          id="panel2a-header"
-        >
-          <Typography className={classes.heading}>Accordion 2</Typography>
+      <Accordion>
+        <AccordionSummary expandIcon={<ExpandMoreIcon/>}>
+          <Typography variant='h5'>如何平等的說話？</Typography>
+          <Typography style={{marginTop: 8, lineHeight: 2}}>
+            西卡神明白社交使人有壓力，使人不敢吐真言。雖然祂是全知全能的，但還是允許信徒隱藏自己的身份說話；祂也知道不是每個人都有機會說出自己的想法，所以祂也允許信徒代為轉述，這一切都是為了讓信徒們能信奉祂，讚頌祂的偉大。
+          </Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-            sit amet blandit leo lobortis eget.
-          </Typography>
+          <Typography style={{marginTop: 8, lineHeight: 2}}>您的密錀是「{randomKey}」，由瀏覽器隨機產生，已存在瀏覽器中，不清快取會一直保留。<br/>您必須使用同樣的「密錀」才能刪改對應的福音。服務器只會紀錄密錀與福音的關聯，不會知道密鑰對應的是誰。</Typography>
         </AccordionDetails>
       </Accordion>
     </div>
@@ -144,7 +152,7 @@ function SimpleCard() {
       />
       <CardContent>
         <Typography variant="h5" component="h2" style={{marginTop: 8}}>
-          你聽過神嗎？
+          你聽過西卡神嗎？
         </Typography>
         <Typography style={{marginTop: 20}}>
           西卡神是博愛的，祂允許所有神、智者、甚至人渣都有平等說話的權利。
@@ -162,19 +170,23 @@ function SimpleCard() {
 
 function MyForm(props) {
   const classes = useStyles()
-  const randomKey = Math.random().toString(36).substring(7);
+
 
   return (
     <Container>
-      <Grid container spacing={2} justify="center" style={{marginTop: 8, justifyContent: 'flex-end', textAlign: 'center'}}>
-          <Grid item>
-            <TextField variant="outlined" margin="dense" value={randomKey} disabled={true}/>
-          </Grid>
-          <Grid item>
-            <Button>
-              重新產生密鑰
-            </Button>
-          </Grid>
+      <Grid container spacing={2} justify="center"
+            style={{marginTop: 8, justifyContent: 'flex-end', textAlign: 'center'}}>
+        <Grid item>
+          <Typography style={{marginTop: 8}}>我的密錀： </Typography>
+        </Grid>
+        <Grid item>
+          <TextField variant="outlined" margin="dense" value={randomKey}/>
+        </Grid>
+        <Grid item>
+          <Button>
+            重新產生密鑰
+          </Button>
+        </Grid>
       </Grid>
       <Typography variant="h4" color="textPrimary" style={{marginTop: 8}}>
         你剛剛聽見－－
@@ -282,7 +294,7 @@ export default function Home(props) {
           {/* Hero unit */}
           <div className={classes.heroContent}>
             <Container maxWidth="md">
-              <Grid container spacing={2}>
+              <Grid container spacing={2} style={{alignItems: 'flex-end'}}>
                 <Grid item xs={6}>
                   <SimpleCard/>
                 </Grid>
@@ -304,7 +316,7 @@ export default function Home(props) {
               >
                 <Tab value={'0'} label="全部"/>
                 <Tab value={'1'} label="我聽見的"/>
-                <Tab value={'2'} label="說明"/>
+                <Tab value={'2'} label="教義說明"/>
               </Tabs>
               <TabPanel value={'0'}>
                 <Grid container spacing={4}>
@@ -395,17 +407,17 @@ export default function Home(props) {
   )
 }
 
-function ScrollTop(props) {
-  const useStyles = makeStyles((theme) => ({
-    root: {
-      position: 'fixed',
-      bottom: theme.spacing(2),
-      right: theme.spacing(2),
-    },
-  }));
+const ScrollTopStyles = makeStyles((theme) => ({
+  root: {
+    position: 'fixed',
+    bottom: theme.spacing(2),
+    right: theme.spacing(2),
+  },
+}));
 
+function ScrollTop(props) {
   const {children, window} = props;
-  const classes = useStyles();
+  const classes = ScrollTopStyles();
   // Note that you normally won't need to set the window ref as useScrollTrigger
   // will default to window.
   // This is only being set here because the demo is in an iframe.
