952339fbe4c0aaea6dbc1a4f29b5d09ba087e4dc
marco79423 <marco79423@gmail.com>
2020-12-12 22:46:26 +0800

    完成 AppBar

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,10 +1,11 @@
 import Button from '@material-ui/core/Button'
+import AppBar from '../components/AppBar'
 
 export default function Index() {
   return (
     <>
       <header>
-
+        <AppBar/>
       </header>
       <main>
         <Button>測試</Button>
