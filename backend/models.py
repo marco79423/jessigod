2a8e3ae4d5ed94e38301e2f020608a372857dea6
marco79423 <marco79423@gmail.com>
2020-12-22 21:49:40 +0800

    新增 created_at 欄位

--- backend/models.py
+++ backend/models.py
@@ -12,6 +12,7 @@ class Editor(Base):
     id = Column(String, primary_key=True)
     token = Column(String, unique=True, index=True)
     sayings = relationship('Saying', back_populates='editor')
+    created_at = Column(DateTime, default=dt.datetime.utcnow)
 
 
 class Origin(Base):
@@ -20,19 +21,16 @@ class Origin(Base):
     id = Column(String, primary_key=True)
     name = Column(String, unique=True, index=True)
     sayings = relationship('Saying', back_populates='origin')
+    created_at = Column(DateTime, default=dt.datetime.utcnow)
 
 
 class Saying(Base):
     __tablename__ = 'saying'
 
     id = Column(String, primary_key=True, index=True)
-
     editor_id = Column(String, ForeignKey('editor.id'))
     editor = relationship('Editor', back_populates='sayings')
-
     origin_id = Column(String, ForeignKey('origin.id'))
     origin = relationship('Origin', back_populates='sayings')
-
     content = Column(Text, unique=True)
-
     created_at = Column(DateTime, index=True, default=dt.datetime.utcnow)
