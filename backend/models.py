e1c3c90a2f03f538452359825165e15880ba077b
marco79423 <marco79423@gmail.com>
2020-12-26 14:53:42 +0800

    調整 model 的 schema

--- backend/models.py
+++ backend/models.py
@@ -10,7 +10,7 @@ class Editor(Base):
     __tablename__ = 'editor'
 
     id = Column(String(36), primary_key=True)
-    token = Column(String, unique=True, index=True)
+    token = Column(String(36), unique=True, index=True)
     sayings = relationship('Saying', back_populates='editor')
     created_at = Column(DateTime, default=dt.datetime.utcnow)
 
@@ -19,7 +19,7 @@ class Origin(Base):
     __tablename__ = 'origin'
 
     id = Column(String(36), primary_key=True)
-    name = Column(String, unique=True, index=True)
+    name = Column(String(64), unique=True, index=True)
     sayings = relationship('Saying', back_populates='origin')
     created_at = Column(DateTime, default=dt.datetime.utcnow)
 
@@ -28,9 +28,9 @@ class Saying(Base):
     __tablename__ = 'saying'
 
     id = Column(String(36), primary_key=True, index=True)
-    editor_id = Column(String, ForeignKey('editor.id'))
+    editor_id = Column(String(36), ForeignKey('editor.id'))
     editor = relationship('Editor', back_populates='sayings')
-    origin_id = Column(String, ForeignKey('origin.id'))
+    origin_id = Column(String(36), ForeignKey('origin.id'))
     origin = relationship('Origin', back_populates='sayings')
     content = Column(Text, unique=True)
     created_at = Column(DateTime, index=True, default=dt.datetime.utcnow)
