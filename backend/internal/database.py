ab7babeb0777da6fec3abeb4a9789445251e8a53
marco79423 <marco79423@gmail.com>
2021-01-08 10:15:04 +0800

    修正斷線重連的問題

--- backend/internal/database.py
+++ backend/internal/database.py
@@ -7,9 +7,9 @@ from config import conf
 SQLALCHEMY_DATABASE_URL = conf.server.database_url
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL
+    SQLALCHEMY_DATABASE_URL,
+    pool_recycle=100,  # 多少時間自動重連 (MySQL 預設會 8 小時踢人)
 )
 SessionLocal = sessionmaker(bind=engine)
 
 Base = declarative_base()
-
