9b81719ffea5c61aa4f333ab072130a5164825b8
marco79423 <marco79423@gmail.com>
2022-05-12 09:37:53 +0800

    feat: 新增 canonical 和網站說明

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -9,6 +9,20 @@ function App({Component, pageProps}) {
         <title>西卡神教福音</title>
 
         <link rel="icon" href="/favicon.ico"/>
+        <link rel="canonical" href="https://jessigod.marco79423.net/"/>
+        <meta name="description" content="西卡神會記錄每個人說的話，並將這些話語平等的傳遞給每一個教徒。"/>
+
+        <meta name="twitter:url" content="https://jessigod.marco79423.net"/>
+        <meta name="twitter:title" content="西卡神教福音"/>
+        <meta name="twitter:description" content="西卡神會記錄每個人說的話，並將這些話語平等的傳遞給每一個教徒。"/>
+        <meta name="twitter:image" content="https://jessigod.marco79423.net/images/god.jpg"/>
+        <meta name="twitter:creator" content="@marco79423"/>
+        <meta property="og:type" content="website"/>
+        <meta property="og:title" content="西卡神教福音"/>
+        <meta property="og:description" content="西卡神會記錄每個人說的話，並將這些話語平等的傳遞給每一個教徒。"/>
+        <meta property="og:site_name" content="西卡神教福音"/>
+        <meta property="og:url" content="https://jessigod.marco79423.net"/>
+        <meta property="og:image" content="https://jessigod.marco79423.net/images/god.jpg"/>
 
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"/>
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
